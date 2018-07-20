640954fccb1040d3cd260b429f05d731fc367a94
gness1804 <graham.nessler@gmail.com>
2018-07-19 21:05:43 -0500

    Refactor redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
-const redis = require('redis');
-const util = require('util');
 const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
-const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+const client = require('../services/redis');
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
---
+++ services/redis.js
@@ -0,0 +1,8 @@
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
+
+module.exports = client;
