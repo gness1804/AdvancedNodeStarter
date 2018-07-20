498ec214fe5f4b3441e2345842707f1ec6117f17
gness1804 <graham.nessler@gmail.com>
2018-07-19 20:58:13 -0500

    Got redis cache working

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
@@ -14,9 +19,23 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+
+    const blogsCacheKey = `${req.user.id}Blog`;
+
+    const cachedBlogs = await client.get(blogsCacheKey);
+
+    if (cachedBlogs) {
+      console.log('serving from cache');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    console.log('serving from mongodb');
+
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
+
+    client.set(blogsCacheKey, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
