9b4ccbd0ea3d72bd14e8e605b3eca23f7e787199
gness1804 <graham.nessler@gmail.com>
2018-07-21 18:13:23 -0500

    WIP: working to get cache working

--- services/cache.js
+++ services/cache.js
@@ -1,35 +1,31 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 const { exec } = mongoose.Query.prototype;
 
-mongoose.Query.prototype.exec = function (...args) {
-  // const key = Object.assign({}, this.getQuery(), {
-  //   collection: this.mongooseCollection.name,
-  // });
-  return exec.apply(this, args);
+mongoose.Query.prototype.exec = async function (...args) {
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  }));
+
+  const matchingCachedData = await client.get(key);
+
+  /* eslint-disable new-cap */
+  // get the data out of redis cache if data exists
+  if (matchingCachedData) {
+    const mongoDoc = new this.model(JSON.parse(matchingCachedData));
+    return mongoDoc;
+  }
+  /* eslint-enable new-cap */
+
+  // runs the actual MongoDB query if no cached data is found
+  // and sets the cache key value in redis
+  const result = await exec.apply(this, args);
+  client.set(key, JSON.stringify(result));
+  return result;
 };
-
-// original boilerplate
-// const redis = require('redis');
-// const util = require('util');
-
-// const redisUrl = 'redis://127.0.0.1:6379';
-// const client = redis.createClient(redisUrl);
-// client.get = util.promisify(client.get);
-
-// module.exports = client;
-
-// from the /blogs get request handler:
-
-// const blogsCacheKey = `${req.user.id}Blog`;
-
-// const cachedBlogs = await client.get(blogsCacheKey);
-
-// if (cachedBlogs) {
-//
-//   return res.send(JSON.parse(cachedBlogs));
-// }
-
-//
-
-// client.set(blogsCacheKey, JSON.stringify(blogs));
