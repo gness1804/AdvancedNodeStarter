0c1709f694e4d25bbf69f5c9fbbe21bf9505d1e1
gness1804 <graham.nessler@gmail.com>
2018-07-21 15:46:18 -0500

    WIP: monkey patching mongoose query

--- services/redis.js
+++ services/redis.js
@@ -1,3 +1,12 @@
+const mongoose = require('mongoose');
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function () {
+
+    return exec.apply(this, arguments);
+}
+
 const redis = require('redis');
 const util = require('util');
 
