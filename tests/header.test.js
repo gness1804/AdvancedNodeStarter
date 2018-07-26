69357fce5af1b98557cc79a6c1113f6573ea019c
gness1804 <graham.nessler@gmail.com>
2018-07-25 19:18:00 -0500

    Add first puppeteer passing test

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,7 +1,14 @@
 /* global it, describe, expect */
+const puppeteer = require('puppeteer');
 
-describe('testing...', () => {
-  it('the test suite works', () => {
-    expect(true).toEqual(true);
+describe('Chromium browser', () => {
+  it('We can launch a new instance of Chromium ', async () => {
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    await page.goto('localhost:3000/');
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    expect(text).toEqual('Blogster');
   });
 });
