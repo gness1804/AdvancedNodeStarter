180dd785110126f9a76f1a23b717f84a9d749227
gness1804 <graham.nessler@gmail.com>
2018-07-25 22:09:41 -0500

    Break out right header links into own component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import RightLinks from './RightLinks';
 
 class Header extends Component {
   renderContent() {
@@ -15,14 +16,7 @@ class Header extends Component {
           </li>
         );
       default:
-        return [
-          <li key="3" style={{ margin: '0 10px' }}>
-            <Link to="/blogs">My Blogs</Link>
-          </li>,
-          <li key="2">
-            <a href={'/auth/logout'}>Logout</a>
-          </li>,
-        ];
+        return <RightLinks />;
     }
   }
 
