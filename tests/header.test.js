3e6a6fbd6b8e46dd87a98b1d653a5f98f5d1b8a2
gness1804 <graham.nessler@gmail.com>
2018-07-25 19:28:16 -0500

    Refactor boilerplate into beforeEach

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,13 +1,18 @@
-/* global it, describe, expect */
+/* global it, describe, expect, beforeEach */
 const puppeteer = require('puppeteer');
 
-describe('Chromium browser', () => {
-  it('We can launch a new instance of Chromium ', async () => {
+describe('Home page', () => {
+  let page;
+
+  beforeEach(async () => {
     const browser = await puppeteer.launch({
       headless: false,
     });
-    const page = await browser.newPage();
+    page = await browser.newPage();
     await page.goto('localhost:3000/');
+  });
+
+  it('has the correct headline text', async () => {
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
   });
