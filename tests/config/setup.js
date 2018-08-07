0937bddc0f58aa267753cce101326dcfbf2592ba
gness1804 <graham.nessler@gmail.com>
2018-08-06 20:16:04 -0500

    Add jest timeout

--- tests/config/setup.js
+++ tests/config/setup.js
@@ -1,6 +1,10 @@
+/* global jest */
+
 require('../../models/User');
 const mongoose = require('mongoose');
 const keys = require('../../config/keys');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
+
+jest.setTimeout(30000);
