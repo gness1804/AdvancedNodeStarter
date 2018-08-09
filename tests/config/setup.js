abde40c37f5922746158941f2d7e641e3cd312c7
gness1804 <graham.nessler@gmail.com>
2018-08-08 21:11:25 -0500

    Add hook to delete all test users each time running tests

--- tests/config/setup.js
+++ tests/config/setup.js
@@ -1,10 +1,12 @@
 /* global jest, beforeAll */
 
 require('../../models/User');
+require('../../models/Blog');
 const mongoose = require('mongoose');
 const keys = require('../../config/keys');
 
 const User = mongoose.model('User');
+const Blog = mongoose.model('Blog');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
@@ -15,4 +17,8 @@ beforeAll(async () => {
   await User.deleteMany({
     displayName: 'Dwayne Johnson',
   });
+  await Blog.deleteMany({
+    title: 'Mal\'s big day out',
+  });
+  process.stdout.write('Deleted all test users and blogs.');
 });
