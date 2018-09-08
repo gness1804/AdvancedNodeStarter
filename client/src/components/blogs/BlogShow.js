99149649140b3fe5377b1be0df26a1d831b4b6c5
gness1804 <graham.nessler@gmail.com>
2018-09-08 11:36:25 -0500

    User can now see blog image

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchBlog } from '../../actions';
 
+
 class BlogShow extends Component {
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
+  }
+
+  showImage() {
+    const { blog: { imageUrl }} = this.props;
+    if (imageUrl) {
+      return <img src={`https://s3.amazonaws.com/blogster-store/${imageUrl}`} alt="The blog image." />
+    }
   }
 
   render() {
@@ -18,6 +26,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.showImage()}
       </div>
     );
   }
